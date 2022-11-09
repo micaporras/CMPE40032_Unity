966a558d12f4cc5437c1ec050c5fbce0819ff16d
Mica Lorraine D. Porras <micalorrainedporras@iskolarngbayan.pup.edu.ph>
2022-11-09 16:58:31 +0800

    Group 5- Challenge 1
    .gitignore
    .gitattributes

---
+++ cpe40032-c1-5/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get the vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Make the plane move forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // Make the plane tilt upwards or downwards
+        transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
+    }
+}
